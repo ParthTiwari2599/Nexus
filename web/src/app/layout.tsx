3ddf7209dcffebd30d0e5ac453a5c5c98e76727c
ParthTiwari2599 <parthtiwari2599@gmail.com>
2026-02-09 00:17:41 +0530

    fix

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,15 +4,20 @@ import { Toaster } from "sonner";
 import ConnectionIndicator from "@/components/ConnectionIndicator";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  const content = (
+    <html lang="en">
+      <body>
+        {children}
+        <Toaster richColors position="bottom-right" />
+        <ConnectionIndicator />
+      </body>
+    </html>
+  );
+
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          {children}
-          <Toaster richColors position="bottom-right" />
-          <ConnectionIndicator />
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+    publishableKey
+      ? <ClerkProvider publishableKey={publishableKey}>{content}</ClerkProvider>
+      : content
+  );
 }
